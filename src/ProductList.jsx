7d8b11feb4d65937ce6dc210d1e701c512dbaf4b
yuxinens <zendhop@gmail.com>
2025-01-21 01:35:13 -0500

    minor update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -280,7 +280,7 @@ function ProductList() {
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="./" style={{ textDecoration: 'none' }}>
+                        <a href="/e-plantShopping/" style={{ textDecoration: 'none' }}>
                             <div>
                                 <h3 style={{ color: 'white' }}>Grennie Shop</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
